0364be5bbedfa7a3658b09d4dc360db5c3566978
Ron Sweeney <ron@basenube.com>
2020-04-09 11:56:59 -0500

    no datapop

--- fhirDB/fhirDB-atelier-project/IRISConfig/mspgo.sh
+++ fhirDB/fhirDB-atelier-project/IRISConfig/mspgo.sh
@@ -6,7 +6,6 @@ intersystems
 zn "%SYS"
 Write \$SYSTEM.OBJ.Load("/home/ec2-user/covid-19-challenge/fhirDB/fhirDB-atelier-project/IRISConfig/Installer.cls","ck")
 w ##class(MSPConfig.FHIRInstaller).DoFHIRServerSetup("HS")
-w ##class(MSPConfig.FHIRInstaller).PopulateFHIRServer("HS")
 w ##class(MSPConfig.FHIRInstaller).SetupIAMUser()
 w ##class(MSPConfig.FHIRInstaller).EnableIAMAPIEndpoint()
 halt
