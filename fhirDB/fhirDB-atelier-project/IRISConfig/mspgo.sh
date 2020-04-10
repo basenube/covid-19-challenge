270492586cd1e82116f69eb13d1413b4b850db15
Ron Sweeney <ron@basenube.com>
2020-04-10 08:37:28 -0500

    changes to stack

--- fhirDB/fhirDB-atelier-project/IRISConfig/mspgo.sh
+++ fhirDB/fhirDB-atelier-project/IRISConfig/mspgo.sh
@@ -1,11 +1,10 @@
 cd /home/ec2-user/covid-19-challenge/fhirDB/
-cp patient-data.tar.gz /tmp/
 /usr/bin/irissession IRIS -U USER <<EOF
 intersystems
 intersystems
 zn "%SYS"
 Write \$SYSTEM.OBJ.Load("/home/ec2-user/covid-19-challenge/fhirDB/fhirDB-atelier-project/IRISConfig/Installer.cls","ck")
-w ##class(MSPConfig.FHIRInstaller).DoFHIRServerSetup("HS")
+w ##class(MSPConfig.FHIRInstaller).DoFHIRServerSetup("USER")
 w ##class(MSPConfig.FHIRInstaller).SetupIAMUser()
 w ##class(MSPConfig.FHIRInstaller).EnableIAMAPIEndpoint()
 halt
